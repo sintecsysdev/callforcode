8b6c149074f8a4d571f6db25779cd94a2a966c36
Eduardo Martins <eduardomartins@MacBook-Pro-de-Eduardo.local>
2018-09-29 03:04:26 -0300

    Bug fixes

--- script/master.js
+++ script/master.js
@@ -20,28 +20,57 @@ $( document ).ready(function() {
 		alert('Zoom -');
 	})
 
+	var current = null;
 
 	//menu draggable
 	$( "#menu" ).draggable();
 
 	//menu
 	$('#fire').click(function(){
+		if (current == 'fire') {
+			$('#video1 source').attr('src', 'videos/1.mp4');
+			$('#video1')[0].load()
+			playPause();
+			current = null;
+			$('.alert_fire').css('display', 'none');
+			return false;
+		}
+
+		current = 'fire';
+
 		console.log('carrega o video 2');
 		$('#video1 source').attr('src','videos/2_alarm.mp4');
 		$('#video1')[0].load()
-		$('.alert_fire').css('display','block');
+		setTimeout(function(){
+			$('.alert_fire').css('display', 'block');
+		}, 1000);
 		playPause();
 
 	});
 	$('.alert_fire').click(function(){
-		$(this).css('display','none')
+		$(this).css('display','none');
+
+		$('#video1 source').attr('src', 'videos/1.mp4');
+		$('#video1')[0].load()
+		playPause();
 	});
 
 	$('#night').click(function(){
-		console.log('carrega o video 3');
+		if (current == 'night') {
+			$('#video1 source').attr('src', 'videos/1.mp4');
+			$('#video1')[0].load()
+			playPause();
+			current = null;
+			return false;
+		}
+	
+		$('.alert_fire').css('display', 'none');
+
+		current = 'night';
 		$('#video1 source').attr('src','videos/3_night.mp4');
 		$('#video1')[0].load()
 	});
+
 	$('#change').click(function(){
 		window.location.href = "4view.html";
 	});
@@ -50,9 +79,10 @@ $( document ).ready(function() {
 	var myVideo = document.getElementById("video1");
 
 	function playPause() {
-			if (myVideo.paused)
+			if (myVideo.paused){
 					myVideo.play();
-			else
+			}else{
 					myVideo.pause();
-	}
+			}
+		}
 });
